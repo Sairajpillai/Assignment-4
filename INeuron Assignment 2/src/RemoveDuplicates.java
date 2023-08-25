586b94f423c1325256bfc9d8f26d26cdd7b8227d
SairajPillai <Sairajpillai8@gmail.com>
2023-08-25 18:54:01 +0530

    2nd commit

--- INeuron Assignment 2/src/RemoveDuplicates.java
+++ INeuron Assignment 2/src/RemoveDuplicates.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-class CheckDuplicate{
-	public void/*char[]*/ removeDuplicate(String str) {
+class CheckDuplicate {
+	public void removeDuplicate(String str) {
 		char res[] = new char[26];
 		char c[] = str.toCharArray();
-		//char res[] = new char[26];
+
 		int counter = 0;
 		int index = 0;
 		for (int i = 0; i < str.length(); i++) {
@@ -18,65 +18,27 @@ class CheckDuplicate{
 			if (i == j) {
 				res[index++] = c[i];
 			}
-			
-		//return res;
-	}
+
+		}
 		if (counter != 0) {
 			System.out.println("After Removing Duplicates");
 			System.out.print(res);
-		}
-		else {
+		} else {
 			System.out.println("No Duplicates are Present");
 		}
-		/*if(counter!=0) {
-			return res;
-		}else {
-			return res;
-		}*/
-	
+
 	}
-	}
+}
+
 public class RemoveDuplicates {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String to remove the duplicates");
 		String str = sc.nextLine();
-		CheckDuplicate c= new CheckDuplicate();
+		CheckDuplicate c = new CheckDuplicate();
 		c.removeDuplicate(str);
-		//char res[] = CheckDuplicate.removeDuplicate(str);
-		//System.out.println(CheckDuplicate.removeDuplicate(str));
-		/*char c[] = str.toCharArray();
-		char res[] = new char[26];
-		int counter = 0;
-		int index = 0;
-		for (int i = 0; i < str.length(); i++) {
-			int j;
-			for (j = 0; j < i; j++) {
-				if (c[i] == c[j]) {
-					counter++;
-					break;
-				}
-			}
-			if (i == j) {
-				res[index++] = c[i];
-			}
 
-		}
-		if (counter != 0) {
-			System.out.println("After Removing Duplicates");
-			System.out.print(res);
-		}
-		else {
-			System.out.println("No Duplicates are Present");
-		}
-		if(res==null) {
-			System.out.println("No duplicates are present");
-		}else {
-			System.out.println("After removing duplicates :");
-			System.out.print(res);
-		}*/
-		
 	}
 
 }
--- INeuron Assignment 2/src/UniqueCharacters.java
+++ INeuron Assignment 2/src/UniqueCharacters.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-class CheckUnique{
-	public void/*char[]*/ removeDuplicate(String str) {
+class CheckUnique {
+	public void removeDuplicate(String str) {
 		char res[] = new char[26];
 		char c[] = str.toCharArray();
-		//char res[] = new char[26];
+
 		int counter = 0;
 		int index = 0;
 		for (int i = 0; i < str.length(); i++) {
@@ -18,24 +18,18 @@ class CheckUnique{
 			if (i == j) {
 				res[index++] = c[i];
 			}
-			
-		//return res;
-	}
+
+		}
 		if (counter != 0) {
 			System.out.println("String does not contains unique charcacters");
-			//System.out.print(res);
-		}
-		else {
+
+		} else {
 			System.out.println("String contains unique characters");
 		}
-		/*if(counter!=0) {
-			return res;
-		}else {
-			return res;
-		}*/
-	
+
 	}
-	}
+}
+
 public class UniqueCharacters {
 
 	public static void main(String[] args) {
@@ -43,7 +37,7 @@ public class UniqueCharacters {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String to check the uniqueness");
 		String str = sc.nextLine();
-		CheckUnique c= new CheckUnique();
+		CheckUnique c = new CheckUnique();
 		c.removeDuplicate(str);
 
 	}
